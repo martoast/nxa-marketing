57e92c2c181fa7e7cca87007bff860f8928886f7
martoast <alexmartos96@gmail.com>
2024-07-23 21:39:58 -0700

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', "@nuxt/image"]
+  modules: ['@nuxtjs/tailwindcss', "@nuxt/image"],
+  image: {
+    provider: 'netlify',
+    domains: ['images.unsplash.com', 'tailwindui.com']
+  }
 })
