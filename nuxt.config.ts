29831f2c0171bc4361faf5d4263c317e5abbc4d7
martoast <alexmartos96@gmail.com>
2024-09-03 08:43:56 -0700

    gtag added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', "@nuxt/image"],
+  modules: ['@nuxtjs/tailwindcss', "@nuxt/image", 'nuxt-gtag'],
   image: {
     provider: 'netlify',
     domains: ['images.unsplash.com', 'tailwindui.com'],
