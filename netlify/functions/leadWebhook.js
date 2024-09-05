d2043e668f23ae393baece926d99e756c22d8056
martoast <alexmartos96@gmail.com>
2024-09-04 17:29:30 -0700

    update webhook url

--- netlify/functions/leadWebhook.js
+++ netlify/functions/leadWebhook.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 
 exports.handler = async (event, context) => {
-  const webhookUrl = 'https://hooks.zapier.com/hooks/catch/19030450/2um8b4l/';
+  const webhookUrl = 'https://hooks.zapier.com/hooks/catch/14797557/2te1ya1/';
 
   const headers = { 'Content-Type': 'application/json' };
 
